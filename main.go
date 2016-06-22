376aeafd0aadee45d6545a66a3e2466b7c76737a
Jeffrey Jen <yihungjen@gmail.com>
2016-06-22 17:34:01 +0000

    Fix template usage and message context recovery

--- main.go
+++ main.go
@@ -34,6 +34,7 @@ func main() {
 	}
 	app.Flags = []cli.Flag{
 		cli.StringFlag{Name: "device", Value: "default", Usage: "Device to Pushbullet"},
+		cli.StringFlag{Name: "email", Value: "", Usage: "Pushbullet to email owner"},
 		cli.BoolFlag{Name: "all", Usage: "Pushbullet all"},
 		cli.StringFlag{Name: "type", Value: push.PUSH_NOTE_TYPE, Usage: "Push payload type"},
 		cli.StringFlag{Name: "title", Usage: "Push title"},
@@ -51,6 +52,10 @@ func main() {
 	}
 	app.Action = func(c *cli.Context) error {
 		var (
+			all    = c.Bool("all")
+			email  = c.String("email")
+			device = c.String("device")
+
 			// Push configuration
 			kind  = c.String("type")
 			title = c.String("title")
@@ -59,6 +64,13 @@ func main() {
 			messageTmpl    = c.Args().Get(0)
 			messageContext = c.Args().Get(1)
 
+			// Blog for holding message context
+			blob interface{} = make(map[string]interface{})
+
+			// Template for outbound message
+			tmpl *template.Template
+
+			// Pushbullet
 			pObj = push.NewPush(kind, title)
 		)
 
@@ -75,47 +87,57 @@ func main() {
 			messageContext = string(data)
 		}
 
-		var tmpl *template.Template
-		var blob interface{}
 		if messageTmpl == "." {
 			tmpl, _ = template.New("payload").Parse(fmt.Sprint("{{ . }}"))
 			// The context is treated as is
 			blob = messageContext
 		} else {
-			t, err := template.New("payload").ParseFiles(messageTmpl)
+			buf, err := ioutil.ReadFile(messageTmpl)
+			if err != nil {
+				return cli.NewExitError("Invalid message template", 1)
+			}
+			t, err := template.New("payload").Parse(string(buf))
 			if err != nil {
 				return cli.NewExitError("Invalid message template", 1)
 			} else {
 				tmpl = t
 			}
 			// The context is a JSON string as argument
-			err = json.Unmarshal([]byte(messageContext), blob)
+			err = json.Unmarshal([]byte(messageContext), &blob)
 			if err != nil {
-				return cli.NewExitError("Invalid message context", 1)
+				// The context is treated as is
+				blob = messageContext
 			}
 		}
 
 		var messageOut = new(bytes.Buffer)
 		if err := tmpl.Execute(messageOut, blob); err != nil {
+			fmt.Println(err)
 			return cli.NewExitError("Unable to genreate message from context", 1)
 		}
+		pObj.Body = messageOut.String()
 
-		ctx, _ := context.WithTimeout(context.Background(), 5*time.Second)
-
-		if c.Bool("all") {
-			pObj.Body = messageOut.String()
-			if err := pObj.Send(ctx); err != nil {
-				return cli.NewExitError(err.Error(), 1)
-			}
-		} else {
-			dev := oauth2.Pushbullet.Has(c.String("device"))
+		switch {
+		default:
+			dev := oauth2.Pushbullet.Has(device)
 			if dev == nil {
 				dev = push.Pushsettings.Default
 			}
 			pObj.Iden = dev.Iden
-			pObj.Body = messageOut.String()
+			break
+		case all == true:
+			break
+		case email != "":
+			pObj.Email = email
+			break
+		}
+
+		for attempts := 0; attempts < 3; attempts++ {
+			ctx, _ := context.WithTimeout(context.Background(), 5*time.Second)
 			if err := pObj.Send(ctx); err != nil {
 				return cli.NewExitError(err.Error(), 1)
+			} else {
+				break // success!
 			}
 		}
 
